ff8c84057e265d1745a3f6d0a8dea9ebe59738d6
Leandro Victor <148922246+leandromoreiira@users.noreply.github.com>
2024-04-11 20:01:39 -0300

    finaização da logica

---
+++ js/script.js
@@ -0,0 +1,64 @@
+let numero1 = 0;
+let numero2 = 0;
+let operador ='';
+
+function escolhenumero(numero){
+    if (operador == ''){
+        numero1 = numero1*10+numero;
+        document.getElementById('number1').innerHTML = numero1;
+    }else{
+        numero2 = numero2 *10 + numero;
+        document.getElementById('number2').innerHTML = numero2;
+    }
+}
+
+
+
+function operacao(op){
+    operador = op;
+}
+
+
+function calcularesultado(){
+    let resultado = 0;
+    switch(operador){
+        case 'soma':
+            resultado = soma(numero1,numero2);
+        break
+        case 'subt':
+            resultado = subt(numero1,numero2);
+        break
+        case 'mult':
+            resultado = mult(numero1,numero2);
+        break
+        case 'divi':
+            resultado = divi(numero1,numero2);
+        break
+            
+    }
+    document.getElementById('resultado').innerHTML = resultado;
+}
+
+
+function soma ( numero1,numero2){
+    return numero1+ numero2;
+}
+function subt ( numero1,numero2){
+    return numero1- numero2;
+}
+function mult ( numero1,numero2){
+    return numero1* numero2;
+}
+function divi ( numero1,numero2){
+    if(numero2 === 0){
+        window.alert('A divisão por 0 não é possivel')
+        return NaN;
+    }else{
+    return numero1/ numero2;
+    }
+}
+
+function limpacampo(){
+    window.location.reload(true);
+
+}
